d2dc685762dde76584aa4b38bfbf3e09cee31999
patelvivek7879 <patelvivek7879@gmail.com>
2024-03-20 16:48:21 +0530

    udpate

--- server.js
+++ server.js
@@ -11,17 +11,17 @@ const connectDB = require('./config/db');
 
 app.use(express.json({extended: false}));
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
+// app.use(express.static(path.resolve(__dirname, './client/build')));
 
 app.use("/api/movies",require("./routes/api/movies"));
 
-if(process.env.NODE_ENV === 'production')
-{
+// if(process.env.NODE_ENV === 'production')
+// {
     app.use(express.static('client/build'));
     app.get('*', (req,res)=>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
-}
+// }
 
 const port = process.env.PORT || 5000;
 app.listen(port,(error)=>{
